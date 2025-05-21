4683464cb7372d0c08ebc97e2766daed4e6b3189
Nahum Alon <nochum.shepsl@gmail.com>
2025-05-21 20:13:09 +0300

    used hook useMemo

--- src/components/Rating.tsx
+++ src/components/Rating.tsx
@@ -1,5 +1,5 @@
 import { HStack } from '@chakra-ui/react';
-import React from 'react'
+import React, { ReactNode, useMemo } from 'react'
 import { FaRegStar, FaStar, FaStarHalfAlt} from 'react-icons/fa';
 
 interface Props {
@@ -8,7 +8,13 @@ interface Props {
     maxRating?: number;
 }
 
-function getStars(rate: number, starsNumber: number = 5,  maxRating: number = 5): { solid: number, half: number, empty: number } {
+interface Stars {
+    solid: number;
+    half: number;
+    empty: number;
+}
+
+function getStarsCounts({rate, starsNumber = 5, maxRating = 5}:Props): Stars {
     const normRating = starsNumber * rate / maxRating;
     let solid = Math.floor(normRating);
     const fractional = normRating - solid;
@@ -21,16 +27,23 @@ function getStars(rate: number, starsNumber: number = 5,  maxRating: number = 5)
         }
     }
     const empty = starsNumber - solid - half;
-    return { solid, half, empty };
+    return {solid, half, empty};
 }
 
-const Rating: React.FC<Props> = ({ starsNumber, rate, maxRating }) => {
-    const { solid, half, empty } = getStars(rate, starsNumber, maxRating);
+function getArrayReactNode({solid, half, empty}: Stars): ReactNode[] {
+    let key = 0;
+    const arr: ReactNode[] = Array.from({length: solid}).map(() => <FaStar key={key++}/>);
+    half == 1 && arr.push(<FaStarHalfAlt key={key++}/>);
+    Array.from({length: empty}).forEach(() => arr.push(<FaRegStar key={key++}/>));
+    return arr;
+}
+
+const Rating: React.FC<Props> = (props) => {
+    
+    const stars = useMemo(() => getArrayReactNode(getStarsCounts(props)), []);
     return (
-        <HStack title={rate.toString()}> 
-            {Array.from({length: solid}).map(() => <FaStar />)}
-            {half == 1 && <FaStarHalfAlt/>}
-            {Array.from({length: empty}).map(() => <FaRegStar />)}
+        <HStack title={props.rate.toString()}> 
+            {stars}
         </HStack>
     );
 }
