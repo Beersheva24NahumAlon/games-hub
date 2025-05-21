5d119fa9c5452b0491e57a507d8ee2eb05652f1f
Nahum Alon <nochum.shepsl@gmail.com>
2025-05-21 20:20:34 +0300

    made some cosmetic changes

--- src/components/Rating.tsx
+++ src/components/Rating.tsx
@@ -1,20 +1,20 @@
 import { HStack } from '@chakra-ui/react';
 import React, { ReactNode, useMemo } from 'react'
-import { FaRegStar, FaStar, FaStarHalfAlt} from 'react-icons/fa';
+import { FaRegStar, FaStar, FaStarHalfAlt } from 'react-icons/fa';
 
 interface Props {
-    starsNumber?: number;
     rate: number;
+    starsNumber?: number;
     maxRating?: number;
 }
 
-interface Stars {
+interface StarsCounts {
     solid: number;
     half: number;
     empty: number;
 }
 
-function getStarsCounts({rate, starsNumber = 5, maxRating = 5}:Props): Stars {
+function getStarsCounts({ rate, starsNumber = 5, maxRating = 5 }: Props): StarsCounts {
     const normRating = starsNumber * rate / maxRating;
     let solid = Math.floor(normRating);
     const fractional = normRating - solid;
@@ -27,23 +27,23 @@ function getStarsCounts({rate, starsNumber = 5, maxRating = 5}:Props): Stars {
         }
     }
     const empty = starsNumber - solid - half;
-    return {solid, half, empty};
+    return { solid, half, empty };
 }
 
-function getArrayReactNode({solid, half, empty}: Stars): ReactNode[] {
+function getArrayReactNode({ solid, half, empty }: StarsCounts): ReactNode[] {
     let key = 0;
-    const arr: ReactNode[] = Array.from({length: solid}).map(() => <FaStar key={key++}/>);
-    half == 1 && arr.push(<FaStarHalfAlt key={key++}/>);
-    Array.from({length: empty}).forEach(() => arr.push(<FaRegStar key={key++}/>));
+    const arr: ReactNode[] = Array.from({ length: solid }).map(() => <FaStar key={key++} />);
+    half == 1 && arr.push(<FaStarHalfAlt key={key++} />);
+    Array.from({ length: empty }).forEach(() => arr.push(<FaRegStar key={key++} />));
     return arr;
 }
 
 const Rating: React.FC<Props> = (props) => {
-    
-    const stars = useMemo(() => getArrayReactNode(getStarsCounts(props)), []);
+    const starsCounts = useMemo(() => getStarsCounts(props), []);
+    const starsNodes = useMemo(() => getArrayReactNode(starsCounts), []);
     return (
-        <HStack title={props.rate.toString()}> 
-            {stars}
+        <HStack title={props.rate.toString()}>
+            {starsNodes}
         </HStack>
     );
 }
