48b9cd236d0a370d40e494116b4ca199fc490383
Nahum Alon <nochum.shepsl@gmail.com>
2025-05-27 12:19:23 +0300

    in store.ts added checks if fields of gameQuery is changed

--- src/state-management/store.ts
+++ src/state-management/store.ts
@@ -19,11 +19,28 @@ interface GameQueryStore {
 }
 
 const useGameQuery = create<GameQueryStore>(set => ({
-    gameQuery: {genreObj:null, platformObj: null, search: null, orderObj: null},
-    setGenre: (genreObj) => set((prevState) => ({gameQuery: {...prevState.gameQuery, genreObj}})),
-    setPlatform: (platformObj) => set((prevState) => ({gameQuery: {...prevState.gameQuery, platformObj}})),
-    setOrder: (orderObj) => set((prevState) => ({gameQuery: {...prevState.gameQuery, orderObj}})),
-    setSearch: (search) => set((prevState) => ({gameQuery: {genreObj: null, platformObj: null, orderObj:prevState.gameQuery.orderObj, search}})),
+    gameQuery: {genreObj: null, platformObj: null, search: null, orderObj: null},
+
+    setGenre: (genreObj) => set((prevState) => ({
+        gameQuery: prevState.gameQuery.genreObj?.slug != genreObj?.slug 
+            ? {...prevState.gameQuery, genreObj} 
+                : prevState.gameQuery
+    })),
+    setPlatform: (platformObj) => set((prevState) => ({
+        gameQuery: prevState.gameQuery.platformObj?.id != platformObj?.id 
+            ? {...prevState.gameQuery, platformObj} 
+                : prevState.gameQuery
+    })),
+    setOrder: (orderObj) => set((prevState) => ({
+        gameQuery: prevState.gameQuery.orderObj?.value != orderObj?.value 
+            ? {...prevState.gameQuery, orderObj} 
+                : prevState.gameQuery
+    })),
+    setSearch: (search) => set((prevState) => ({
+        gameQuery: prevState.gameQuery.search != search 
+            ? {genreObj: null, platformObj: null, orderObj:prevState.gameQuery.orderObj, search} 
+                : prevState.gameQuery
+    })),
 }));
 
 export default useGameQuery;
